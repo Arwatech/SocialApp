aa87c56c73e83f9988f6c486cd732f198c11af91
Arwatech <rookaturki@hotmail.com>
2022-08-05 15:18:23 +0300

    Initial Commit

---
+++ SocialApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialApp
+//
+//  Created by Arwa Alzahrani on 07/01/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SocialApp/SocialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialAppApp.swift
+//  SocialApp
+//
+//  Created by Arwa Alzahrani on 07/01/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SocialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
